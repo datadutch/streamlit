83bc549ce75079cfc663ea4fc90f6e52e6f6f616
OokJohan <11424878+bidutch@users.noreply.github.com>
2023-03-30 18:57:48 +0200

    snowflake

--- code_chatgpt.py
+++ code_chatgpt.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
+import snowflake.connector
+
+@st.cache_resource
+def init_connection():
+    return snowflake.connector.connect(
+        **st.secrets["snowflake"], client_session_keep_alive=True
+    )
+
+conn = init_connection()
 
 text_input = st.text_input('Enter your text here')
 df = pd.DataFrame({'Text': [text_input]})
 st.write(df)
+
+snowflake.connector.pandas_tools.write_pandas(conn, df, '<table>')
